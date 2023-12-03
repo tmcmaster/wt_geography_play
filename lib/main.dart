783398867993ae409d435639abd4320355ac302f
Tim McMaster <tim@mcmaster.id.au>
2023-12-03 16:36:58 +1100

    upgraded libraries

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:wt_app_scaffold/app_platform/scaffold_app_dsl.dart';
 import 'package:wt_app_scaffold/app_scaffolds.dart';
 import 'package:wt_firepod/wt_firepod.dart';
 import 'package:wt_geography_play/apps/explore_map/explore_map_app.dart';
@@ -12,74 +12,69 @@ import 'package:wt_geography_play/firebase_options.dart';
 void main() async {
   runMyApp(
     withFirebase(
-      andAppScaffold(
-        appDetails: appDetails,
-        appDefinition: appDefinition,
-        loginSupport: const LoginSupport(
-          googleEnabled: false,
-          emailEnabled: true,
+      andFirebaseLogin(
+        andAppScaffold(
+          appDetails: appDetails,
+          appDefinition: appDefinition,
+          appStyles: SharedAppConfig.styles,
         ),
+        emailEnabled: true,
       ),
+      database: true,
       appName: 'wt-geography-play',
       firebaseOptions: DefaultFirebaseOptions.currentPlatform,
     ),
   );
 }
 
-final appDetails = Provider<AppDetails>(
-  name: 'AppDefinition',
-  (ref) => AppDetails(
-    title: 'Geography Play',
-    subTitle: 'Explore your interests',
-    iconPath: 'assets/wt_logo_3.png',
-  ),
+final appDetails = AppDetails(
+  title: 'Geography Play',
+  subTitle: 'Explore your interests',
+  iconPath: 'assets/wt_logo_3.png',
 );
 
-final appDefinition = Provider<AppDefinition>(
-  name: 'AppDefinition',
-  (ref) => AppDefinition.from(
-    appName: 'geography-play',
-    appTitle: 'Geography Play',
-    includeAppBar: false,
-    swipeEnabled: false,
-    pages: [
-      PageDefinition(
-        title: 'Find Dinosaurs',
-        icon: FontAwesomeIcons.magnifyingGlass,
-        builder: (context) => const FindDinosaursApp(),
-        primary: true,
-      ),
-      PageDefinition(
-        title: 'Explore Map',
-        icon: Icons.explore,
-        builder: (context) => const ExploreMapApp(),
-        primary: true,
-      ),
-      PageDefinition(
-        title: 'Navigate Between',
-        icon: Icons.navigation_rounded,
-        builder: (context) => const NavigateBetweenApp(),
-        primary: true,
-      ),
-      PageDefinition(
-        title: 'Find Country',
-        icon: Icons.find_in_page,
-        builder: (context) => const FindCountryApp(),
-        primary: true,
-      ),
-      // TODO: Need to implement this game.
-      PageDefinition(
-        title: 'Guess Country',
-        icon: Icons.find_in_page,
-        builder: (context) => const ExploreMapApp(),
-        primary: true,
-      ),
-      PageDefinition(
-        title: 'Settings',
-        icon: Icons.settings,
-        builder: (context) => const SettingsPage(),
-        primary: true,
-      ),
-    ],
-  ),
+final appDefinition = AppDefinition.from(
+  appName: 'geography-play',
+  appTitle: 'Geography Play',
+  includeAppBar: false,
+  swipeEnabled: false,
+  pages: [
+    PageDefinition(
+      title: 'Find Dinosaurs',
+      icon: FontAwesomeIcons.magnifyingGlass,
+      builder: (context) => const FindDinosaursApp(),
+      primary: true,
+    ),
+    PageDefinition(
+      title: 'Explore Map',
+      icon: Icons.explore,
+      builder: (context) => const ExploreMapApp(),
+      primary: true,
+    ),
+    PageDefinition(
+      title: 'Navigate Between',
+      icon: Icons.navigation_rounded,
+      builder: (context) => const NavigateBetweenApp(),
+      primary: true,
+    ),
+    PageDefinition(
+      title: 'Find Country',
+      icon: Icons.find_in_page,
+      builder: (context) => const FindCountryApp(),
+      primary: true,
+    ),
+    // TODO: Need to implement this game.
+    PageDefinition(
+      title: 'Guess Country',
+      icon: Icons.find_in_page,
+      builder: (context) => const ExploreMapApp(),
+      primary: true,
+    ),
+    PageDefinition(
+      title: 'Settings',
+      icon: Icons.settings,
+      builder: (context) => const SettingsPage(),
+      primary: true,
+    ),
+  ],
 );
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,32 +5,36 @@
 import FlutterMacOS
 import Foundation
 
-import cloud_functions
 import desktop_webview_auth
-import facebook_auth_desktop
+import device_info_plus
 import firebase_auth
 import firebase_core
+import firebase_crashlytics
 import firebase_database
-import firebase_remote_config
-import flutter_secure_storage_macos
+import firebase_storage
+import flutter_localization
 import geolocator_apple
+import package_info_plus
+import path_provider_foundation
 import shared_preferences_foundation
 import sign_in_with_apple
-import twitter_login
 import url_launcher_macos
+import webview_universal
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FLTFirebaseFunctionsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFunctionsPlugin"))
   DesktopWebviewAuthPlugin.register(with: registry.registrar(forPlugin: "DesktopWebviewAuthPlugin"))
-  FacebookAuthDesktopPlugin.register(with: registry.registrar(forPlugin: "FacebookAuthDesktopPlugin"))
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
   FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
-  FLTFirebaseRemoteConfigPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseRemoteConfigPlugin"))
-  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+  FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
+  FlutterLocalizationPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalizationPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
-  TwitterLoginPlugin.register(with: registry.registrar(forPlugin: "TwitterLoginPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  WebviewUniversalPlugin.register(with: registry.registrar(forPlugin: "WebviewUniversalPlugin"))
 }
